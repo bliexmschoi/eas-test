1aeb75d7abaf7374f0fa2b4f6a0a691714a09a36
antonchoi <anton.choi@ppoint.kr>
2025-10-29 15:54:58 +0900

    test 빌드 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! 1</ThemedText>
+        <ThemedText type="title">Welcome! 2</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
